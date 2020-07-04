3db2e06b844b2ab31e523b743b9c86403147e690
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-04 23:54:16 +0530

    working code

---
+++ src/main/java/yb/yadnyesh/internet/tutopoint/MyFirstJavaProgram.java
@@ -0,0 +1,21 @@
+package yb.yadnyesh.internet.tutopoint;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * My <b>First Java Program</b>.
+ *
+ * @see Annotation
+ */
+
+ 
+public final class MyFirstJavaProgram {
+
+    private MyFirstJavaProgram(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java 14");
+    }
+}
